169a556688ee0d308e76a71e7aca098f80c75607
Heather Olcot <hfolcot@gmail.com>
2019-08-22 08:41:09 +0100

    setup local env, added base html, added welcome html and basic session

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 import os
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, session, request, redirect, url_for
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
+app.secret_key = os.environ.get('SECRET_KEY')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    flash("Hello")
+    if request.method == 'POST':
+        username = request.form["username"]
+        session['username'] = username
+        return redirect(url_for('start_session', user = username))
     return render_template("index.html")
 
+@app.route('/welcome/<user>')
+def start_session(user):
+    return render_template('welcome.html', user=user)
+
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",
-    port="5000",
+    app.run(host=os.environ.get('IP'),
+    port=os.environ.get('PORT'),
     debug = True)
